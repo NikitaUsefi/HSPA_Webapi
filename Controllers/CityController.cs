0a10de8dc5a2de5281afca68be0a3b71d0af7f41
nikitausefi <Nikitausefi@gmail.com>
2021-01-19 18:56:46 +0330

    Input validation and exception handling in Asp.net web Api

--- Controllers/CityController.cs
+++ Controllers/CityController.cs
@@ -46,12 +46,21 @@ namespace WebAPI.Controllers
         [HttpPut("update/{id}")]
         public async Task<IActionResult> UpdateCity(int id,CityDto cityDto)
         {
-            var cityFromDb = await unitOfWork.CityRepository.FindCity(id);
-            cityFromDb.LastUpdatedBy = 1;
-            cityFromDb.LastUpdatedOn = DateTime.Now;
-            mapper.Map(cityDto, cityFromDb);
-            await unitOfWork.SaveAsync();
-            return StatusCode(200);
+            try
+            {
+                var cityFromDb = await unitOfWork.CityRepository.FindCity(id);
+                if (cityFromDb == null)
+                    return BadRequest("update not allowed");
+                cityFromDb.LastUpdatedBy = 1;
+                cityFromDb.LastUpdatedOn = DateTime.Now;
+                mapper.Map(cityDto, cityFromDb);
+                await unitOfWork.SaveAsync();
+                return StatusCode(200);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Some unknown error has occured");
+            }
 
         }
         [HttpPut("updatecityname/{id}")]
